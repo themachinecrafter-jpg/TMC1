ccd37dbab7821b939777ccfe7b83a1c13d02a956
themachinecrafter-jpg <themachinecrafter@gmail.com>
2026-04-29 02:35:37 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import Replicate from "replicate";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+app.get("/", (req, res) => {
+  res.send("XTTS API is running");
+});
+
+app.post("/tts", async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    const output = await replicate.run("coqui/xtts-v2", {
+      input: {
+        text: text,
+        speaker: "female_en_1",
+        language: "en"
+      }
+    });
+
+    res.json({ audio: output });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server running"));
